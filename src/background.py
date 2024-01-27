5966de98cce9334aab988dfc5aa01ccecca8391b
Marina-Banov <m.banov7@gmail.com>
2024-01-27 09:01:01 +0100

    colors

--- src/background.py
+++ src/background.py
@@ -6,22 +6,22 @@ from .constants import *
 class Background:
     def __init__(self):
         self.bg_images = []
-        for i in range(1, 6):
-            if i == 5:
-                continue
-            bg_image = pygame.image.load(f"{ASSETS_BG_FOLDER}/parallax_0/{i}.png").convert_alpha()
+        for i in range(6):
+            bg_image = pygame.image.load(f"{ASSETS_BG_FOLDER}/parallax_0/{i+1}.png").convert_alpha()
             bg_image = pygame.transform.scale(bg_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
             self.bg_images.append(bg_image)
         self.width = self.bg_images[0].get_width()
         self.scroll = 0
 
     def update(self):
-        if self.scroll < self.width * len(self.bg_images) / 2:
-            self.scroll += 3
+        self.scroll += 3
 
     def draw(self, screen):
-        for n in range(5):
+        for n in range(10):
             speed = 1
             for i, image in enumerate(self.bg_images):
-                screen.blit(image, (n * self.width - self.scroll * speed, 0))
+                pos = n * self.width - self.scroll * speed
                 speed += 0.2
+                speed = round(speed, 2)
+                if -SCREEN_WIDTH < pos < SCREEN_WIDTH:
+                    screen.blit(image, (pos, 0))
