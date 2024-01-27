030b1f794392b781b665dd53e2826cee9e67c0c0
Marina-Banov <m.banov7@gmail.com>
2024-01-27 13:13:26 +0100

    infinite background parallax

--- src/background.py
+++ src/background.py
@@ -12,16 +12,21 @@ class Background:
             self.bg_images.append(bg_image)
         self.width = self.bg_images[0].get_width()
         self.scroll = 0
+        self.n_layers = [0, 0, 0, 0, 0, 0]
+        self.pos = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        self.speeds = [1.0, 1.2, 1.4, 1.6, 1.8, 2.0]
 
     def update(self):
         self.scroll += 3
+        for i, image in enumerate(self.bg_images):
+            pos = self.n_layers[i] * self.width - self.scroll * self.speeds[i]
+            if -SCREEN_WIDTH < pos < SCREEN_WIDTH:
+                self.pos[i] = pos
+            else:
+                self.n_layers[i] += 1
+                self.pos[i] = self.n_layers[i] * self.width - self.scroll * self.speeds[i]
 
     def draw(self, screen):
-        for n in range(10):
-            speed = 1
-            for i, image in enumerate(self.bg_images):
-                pos = n * self.width - self.scroll * speed
-                speed += 0.2
-                speed = round(speed, 2)
-                if -SCREEN_WIDTH < pos < SCREEN_WIDTH:
-                    screen.blit(image, (pos, 0))
+        for i, image in enumerate(self.bg_images):
+            screen.blit(image, (self.pos[i], 0))
+            screen.blit(image, (self.pos[i] + SCREEN_WIDTH, 0))
