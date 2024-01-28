9c65611895fc65da7eb45c84c685802b08ffe682
Sanko <rasanko@exevio.com>
2024-01-28 15:39:30 +0100

    add clock to game

--- main.py
+++ main.py
@@ -11,7 +11,8 @@ class GGJ_2024:
         self.screen = pygame.display.set_mode((constants.SCREEN_WIDTH, constants.SCREEN_HEIGHT))
         pygame.display.set_caption("GGJ 2024")
         self.clock = pygame.time.Clock()
-        self.game = Game()
+        self.game = Game(self.clock)
+        pygame.mixer.init()
 
     def game_loop(self):
         while True:
